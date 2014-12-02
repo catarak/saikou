12aee23645518b0a6197afdd60457c0a28def083
github username <github email address>
2014-12-02 14:29:37 -0500

    Update musicline scraper

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   has_many :records
+  has_many :weeks, through: :records
   has_many :charts, through: :records
   validates_uniqueness_of :name, scope: :artist_id
 end
