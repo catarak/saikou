cf8ec28ef3e52eb549beb0b823a10bf2cfb09514
catarak <ctarakajian@gmail.com>
2014-12-02 18:46:07 -0500

    initial working draft

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,5 +1,7 @@
 class Api::SongsController < ApplicationController
   def index
-
+    @week = Week.find(params[:week_id])
+    @records = Record.where(week_id: @week.id)
+    @countries = Country.joins(charts: :records).where(records: {week_id: @week.id})
   end
 end
