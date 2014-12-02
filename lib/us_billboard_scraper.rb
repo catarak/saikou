e98bc04619b7c36854eaaf63af88f5cc33e2155a
catarak <ctarakajian@gmail.com>
2014-12-02 11:30:07 -0500

    scraping works for a year

--- lib/us_billboard_scraper.rb
+++ lib/us_billboard_scraper.rb
@@ -9,9 +9,6 @@ class UsBillboardScraper
 
   def scrape
     current_week = date_of_next("Saturday")
-    week_str = current_week.strftime("%Y-%m-%d")
-    week_num = current_week.strftime("%U").to_i
-    year_num = current_week.year
     still_scraping = true
     while (still_scraping) do
       begin
@@ -21,14 +18,11 @@ class UsBillboardScraper
 
         #find or create by everything
         year = Year.find_or_create_by(number: current_week.year)
-        week = Week.find_or_create_by(number: current_week.strftime("%U").to_i)
-        WeekYear.find_or_create_by(week_id: week.id, year_id: year.id)
-        WeekCountry.find_or_create_by(week_id: week.id, country_id: @country.id)
+        week = Week.find_or_create_by(number: current_week.strftime("%U").to_i, year_id: year.id)
 
         artist = Artist.find_or_create_by(name: artist_name)
         song = Song.create(name: song_name, artist_id: artist.id)
-        #SongChart.create(song_id: song.id, chart_id: @chart.id)
-        ChartWeek.create(week_id: week.id, chart_id: @chart.id, song_id: song.id)
+        Record.create(week_id: week.id, chart_id: @chart.id, song_id: song.id)
 
         current_week = current_week - 1.week
         if year.number == 2013
