1701ab22382fb4e37a307e794b6f5a4c28077e9c
github username <github email address>
2014-12-05 10:15:46 -0500

    Old Japanese scraper updates

--- lib/jp_oricon_scraper.rb
+++ lib/jp_oricon_scraper.rb
@@ -9,7 +9,8 @@ class JpOriconScraper
   end
 
   def scrape
-    @date = date_of_recent("Monday")
+    # @date = date_of_recent("Monday")
+    @date = Date.new(2009,8,31)
     @end_date = Date.new(2005,10,10)
     while @date > @end_date
       parse_oricon_page
@@ -31,6 +32,8 @@ class JpOriconScraper
       @db_date = Date.parse(@db_date.gsub(/./) { |m| symbol_replace.fetch(m,m) } )
     rescue OpenURI::HTTPError => the_error
       @db_date = @db_date - 7.days
+    rescue Exception => e
+      @date = @date + 7.days
     end
     
     #Creating the year and week
