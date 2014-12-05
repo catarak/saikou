535f93623b492e9720fad7f6dbd242e236786e2e
catarak <ctarakajian@gmail.com>
2014-12-05 15:03:32 -0500

    fixed video query

--- app/assets/javascripts/youtube.js
+++ app/assets/javascripts/youtube.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   // user clicks button with cursor
   $(".countries").on("click", ".country-container", function() {
     var song = $(this).children(".song").text();
-    var artist = $(this).children(".artist").text();
-    var query = song + " " + artist;
+    var artist = $(this).children(".artist").text().slice(3);
+    var query = artist + " - " + song;
     videoSearch(query);
   });
 
